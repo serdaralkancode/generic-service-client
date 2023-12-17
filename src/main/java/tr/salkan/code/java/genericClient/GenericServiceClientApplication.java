645d798e433efb152819239e7f50cc15922c709a
Serdar <serdaralkan_@hotmail.com>
2023-12-18 01:12:01 +0300

    generic-service-client code

---
+++ src/main/java/tr/salkan/code/java/genericClient/GenericServiceClientApplication.java
@@ -0,0 +1,13 @@
+//package tr.salkan.code.java.genericClient;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+//public class GenericServiceClientApplication {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(GenericServiceClientApplication.class, args);
+//	}
+//
+//}
---
+++ src/test/java/tr/salkan/code/java/genericClient/GenericServiceClientApplicationTests.java
@@ -0,0 +1,13 @@
+//package tr.salkan.code.java.genericClient;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class GenericServiceClientApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
